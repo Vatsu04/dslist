908bdbf80d24fec22a1d7aaa89182898f2bc87ff
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-05-20 20:48:35 -0300

    Aula 2 parte 4

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,14 +1,14 @@
 package com.devsuperior.dslist.services;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -16,6 +16,7 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
     
+    @Transactional(readOnly = true)
     public GameDTO findById(Long id) {
     	Game result = gameRepository.findById(id).get();
     	return new GameDTO(result);
@@ -23,8 +24,9 @@ public class GameService {
     	
     }
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
-        return result.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
