c988f655474828125e44e9e330a400f2a4728207
Gustavo Passos Tascheri <98766671+Vatsu04@users.noreply.github.com>
2025-05-20 17:59:08 -0300

    Update

--- src/main/java/com/devsuperior/dslist/entities/Belonging.java
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -1,8 +1,23 @@
 package com.devsuperior.dslist.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
 public class Belonging {
-	private BelongingPK id;
+	private BelongingPK id = new BelongingPK();
 	private Integer position;
 	
+	public Belonging() {
+		
+	}
+
+	public Belonging(Game game, GameList list, Integer position) {
+
+		id.setGame(game);
+		id.setList(list);
+		this.position = position;
+	}
 	
 }
